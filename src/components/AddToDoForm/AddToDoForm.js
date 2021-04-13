b4007b43404df40648d1e28a5bb2e0a4fd4defe7
Patty9049 <gumis9049@gmail.comgit>
2021-04-13 17:01:38 +0200

    Add react-flexbox-grid

--- src/components/AddToDoForm/AddToDoForm.js
+++ src/components/AddToDoForm/AddToDoForm.js
@@ -1,25 +1,28 @@
 import React from "react";
+import { Grid } from "react-flexbox-grid";
 
 const AddToDoForm = ({ addToDo }) => {
   return (
-    <form onSubmit={addToDo}>
-      <input type="text" name="toDoTitle" placeholder="Type to do title.." />
-      <textarea
-        type="text"
-        name="toDoContent"
-        placeholder="Type to do content.."
-        cols="30"
-        rows="10"
-      />
-      <label htmlFor="toDoPriority">Choose todo priority: </label>
-      <select name="toDoPriority" id="toDoPriority">
-        <option value="low">low</option>
-        <option value="medium">medium</option>
-        <option value="high">high</option>
-      </select>
+    <Grid>
+      <form onSubmit={addToDo}>
+        <input type="text" name="toDoTitle" placeholder="Type to do title.." />
+        <textarea
+          type="text"
+          name="toDoContent"
+          placeholder="Type to do content.."
+          cols="30"
+          rows="10"
+        />
+        <label htmlFor="toDoPriority">Choose todo priority: </label>
+        <select name="toDoPriority" id="toDoPriority">
+          <option value="low">low</option>
+          <option value="medium">medium</option>
+          <option value="high">high</option>
+        </select>
 
-      <button type="submit">add todo</button>
-    </form>
+        <button type="submit">add todo</button>
+      </form>
+    </Grid>
   );
 };
 
